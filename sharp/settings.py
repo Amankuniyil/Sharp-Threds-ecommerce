b7afd78b505e6901b7bbd871ca22c6ee7f84436c
Aman Ahmed <amanahmedbarca@gmail.com>
2023-05-17 16:58:44 +0530

    undoCmnt

--- sharp/settings.py
+++ sharp/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'sharp.middleware.AuthMiddleware',
+    'sharp.middleware.AuthMiddleware',
 
     
 ]
